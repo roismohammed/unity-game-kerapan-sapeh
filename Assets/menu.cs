ce98dc08ec09a2136a074fe4286d693bd6d240db
Muhammad Rois <id.roismohammed@gmail.com>
2026-05-24 20:21:41 +0700

    update button

--- Assets/menu.cs
+++ Assets/menu.cs
@@ -1,53 +1,128 @@
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+
 public class menu : MonoBehaviour
 {
+    [Header("Main Panel")]
     public GameObject menuPanel;
     public GameObject materiPanel;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    [Header("Materi Panel")]
+    public GameObject sejarahPanel;
+    public GameObject peralatanPanel;
+    public GameObject peraturanPanel;
+    public GameObject nilaiBudayaPanel;
+
     void Start()
     {
+        // Panel awal
         menuPanel.SetActive(true);
         materiPanel.SetActive(false);
+
+        // Materi awal
+        sejarahPanel.SetActive(true);
+        peralatanPanel.SetActive(false);
+        peraturanPanel.SetActive(false);
+        nilaiBudayaPanel.SetActive(false);
     }
 
-
-    void Update()
-    {
-        
-    }
-
+    // START GAME
     public void startgame(string scenename)
     {
         SceneManager.LoadScene(scenename);
     }
 
+    // BUKA MATERI
     public void infoButton()
     {
         menuPanel.SetActive(false);
         materiPanel.SetActive(true);
     }
 
-    public void showMenuPanel()
-    {
-        menuPanel.SetActive(true);
-        materiPanel.SetActive(false);
-    }
-
+    // KEMBALI KE MENU
     public void backButton()
     {
         menuPanel.SetActive(true);
         materiPanel.SetActive(false);
     }
 
+    // KELUAR GAME
     public void exitgame()
     {
         Application.Quit();
     }
 
+    // AUDIO
     public void AudioButton()
     {
-        
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+    // =========================
+    // BUTTON MATERI
+    // =========================
+
+    public void ShowSejarah()
+    {
+        sejarahPanel.SetActive(true);
+        peralatanPanel.SetActive(false);
+        peraturanPanel.SetActive(false);
+        nilaiBudayaPanel.SetActive(false);
+    }
+
+    public void ShowPeralatan()
+    {
+        sejarahPanel.SetActive(false);
+        peralatanPanel.SetActive(true);
+        peraturanPanel.SetActive(false);
+        nilaiBudayaPanel.SetActive(false);
+    }
+
+    public void ShowPeraturan()
+    {
+        sejarahPanel.SetActive(false);
+        peralatanPanel.SetActive(false);
+        peraturanPanel.SetActive(true);
+        nilaiBudayaPanel.SetActive(false);
+    }
+
+    public void ShowNilaiBudaya()
+    {
+        sejarahPanel.SetActive(false);
+        peralatanPanel.SetActive(false);
+        peraturanPanel.SetActive(false);
+        nilaiBudayaPanel.SetActive(true);
+    }
+
+    public void nextButton()
+    {
+        if (sejarahPanel.activeSelf)
+        {
+            ShowPeralatan();
+        }
+        else if (peralatanPanel.activeSelf)
+        {
+            ShowPeraturan();
+        }
+        else if (peraturanPanel.activeSelf)
+        {
+            ShowNilaiBudaya();
+        }
+    }
+
+    public void prevButton()
+    {
+        if (nilaiBudayaPanel.activeSelf)
+        {
+            ShowPeraturan();
+        }
+        else if (peraturanPanel.activeSelf)
+        {
+            ShowPeralatan();
+        }
+        else if (peralatanPanel.activeSelf)
+        {
+            ShowSejarah();
+        }
     }
 }
